64ca4d2ed47fc2023153cb2f5c93954671eb9452
Mario victor <mariomvmv7777@gmail.com>
2026-04-16 10:38:24 -0300

    green: implementa validateTitle

--- src/addTask.js
+++
@@ -1,8 +0,0 @@
-export function addTask(tasks, title) {
-  if (!validateTitle(title)) {
-    throw new Error('Título inválido');
-  }
-
-  const newTask = createTask(title);
-  return [...tasks, newTask];
-}
